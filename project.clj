0bc37709fd51f0d108d145f20af724124937de72
Karl Sutt <karl@sutt.ee>
2013-09-15 22:53:58 +0100

    Add the Seesaw GUI framework

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [seesaw "1.4.3"]]
   :profiles {:dev {:dependencies [[speclj "2.7.5"]]}}
   :plugins [[speclj "2.7.5"]]
   :test-paths ["spec/"]
