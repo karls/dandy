432a83dda59d1b774e48d4d40451ee51cd1398fe
Karl Sutt <karl@sutt.ee>
2013-09-22 18:14:42 +0200

    Horribly overfitted preferences for LVM.

--- src/dandy/prefs.clj
+++ src/dandy/prefs.clj
@@ -7,9 +7,11 @@
 
 (def prefs (prefs/preference-atom "prefs" {}))
 
+(def directory-label
+  (s/label :text "Images directory"))
+
 (def directory-text-field
   (s/text :text (get @prefs :dir)
-          :columns 20
           :enabled? false))
 
 (defn dir-chosen [fc file]
@@ -18,43 +20,88 @@
     (s/text! directory-text-field path)))
 
 (def directory-button
-  (s/button :text "Choose"
+  (s/button :text "..."
             :listen [:action (fn [e]
                                (choose-file :type :open
                                             :selection-mode :dirs-only
                                             :dir (get @prefs :dir)
                                             :success-fn dir-chosen))]))
+(def new-logo-label
+  (s/label :text "'New logo' image"))
 
-(def layers-panel (s/horizontal-panel :border 5))
+(def new-logo-text-field
+  (s/text :text (get @prefs :new-logo)
+          :enabled? false))
 
-(defn layer-chooser []
-  (choose-file :type :open :selection-mode :files-only))
+(defn new-logo-chosen [fc file]
+  (let [path (.getAbsolutePath file)]
+    (reset! prefs (conj @prefs { :new-logo path }))
+    (s/text! new-logo-text-field path)))
 
-(def add-layer-pane
-  (mig-panel :items [[(s/text :text "Name" :columns 10) ""]
-                     [(s/button :text "Choose layer" :listen [:action (fn [e] (layer-chooser))]) ""]
-                     [(s/button :text "Add") ""]]))
+(def new-logo-button
+  (s/button :text "..."
+            :listen [:action (fn [e]
+                               (choose-file :type :open
+                                            :selection-mode :files-only
+                                            :success-fn new-logo-chosen))]))
+(def new-price-label
+  (s/label :text "'New price' image"))
 
-(defn make-layers-tab []
-  (s/vertical-panel :items ["foo" "bar" "baz" "qux" "quuz"]))
-  ;; (seesaw.forms/forms-panel :items ["Login" (s/text) (seesaw.forms/next-line)
-  ;;                                   "Password" (seesaw.forms/span (s/text) 3)]))
-  ;; (mig-panel :items [[layers-panel "growy, wrap"]
-  ;;                    [add-layer-pane ""]]))
+(def new-price-text-field
+  (s/text :text (get @prefs :new-price)
+          :enabled? false))
 
-(defn make-default-directory-tab []
-  (mig-panel :items [[directory-text-field ""]
-                     [directory-button ""]]))
+(defn new-price-chosen [fc file]
+  (let [path (.getAbsolutePath file)]
+    (reset! prefs (conj @prefs { :new-price path }))
+    (s/text! new-price-text-field path)))
 
-(defn make-preference-tabs []
-  (mig-panel :items [[(s/tabbed-panel
-                       :tabs [{ :title "Default directory" :content (make-default-directory-tab) }
-                              { :title "Layers" :content (make-layers-tab) }])
-                      "grow, shrink"]]))
+(def new-price-button
+  (s/button :text "..."
+            :listen [:action (fn [e]
+                               (choose-file :type :open
+                                            :selection-mode :files-only
+                                            :success-fn new-price-chosen))]))
+(def new-label
+  (s/label :text "'New' image"))
+
+(def new-text-field
+  (s/text :text (get @prefs :new)
+          :enabled? false))
+
+(defn new-chosen [fc file]
+  (let [path (.getAbsolutePath file)]
+    (reset! prefs (conj @prefs { :new path }))
+    (s/text! new-text-field path)))
+
+(def new-button
+  (s/button :text "..."
+            :listen [:action (fn [e]
+                               (choose-file :type :open
+                                            :selection-mode :files-only
+                                            :success-fn new-chosen))]))
+
+(defn make-preference-content []
+  (mig-panel :items [[directory-label "align right"]
+                     [directory-text-field "width 300px"]
+                     [directory-button "wrap 30px"]
+
+                     [new-logo-label "align right"]
+                     [new-logo-text-field "growx"]
+                     [new-logo-button "wrap"]
+
+                     [new-price-label "align right"]
+                     [new-price-text-field "growx"]
+                     [new-price-button "wrap"]
+
+                     [new-label "align right"]
+                     [new-text-field "growx"]
+                     [new-button ""]]))
+  
 
 (defn make-prefs-dialog []
   (let [frame (s/frame :title "Preferences"
                        :on-close :dispose
                        :resizable? false)]
-    (s/config! frame :content (make-preference-tabs))
+    (s/config! frame :content (make-preference-content))
     (-> frame s/pack! s/show!)))
