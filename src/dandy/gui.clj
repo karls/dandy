2e544b143def22444903ceb4202395e11e6306a6
Karl Sutt <karl@sutt.ee>
2013-09-22 17:48:59 +0200

    Broken prefs

--- src/dandy/gui.clj
+++ src/dandy/gui.clj
@@ -1,17 +1,30 @@
 (ns dandy.gui
   (require [seesaw.core :as s]
-           [dandy.prefs :as prefs]
-           [seesaw.chooser :as chooser]))
+           [dandy.prefs :as prefs])
+  (use [seesaw.chooser :only (choose-file)]))
+
+(def status-text (s/label :text "Add files to convert"
+                          :foreground "#444"
+                          :font :monospaced
+                          :v-text-position :bottom
+                          :border (seesaw.border/line-border :top 1 :color "#BBB")))
+
+(def convert-button (s/button :text "Convert files" :enabled? false))
+
+(defn files-selected [fc, files]
+  (s/text! status-text (str (count files) " files ready to be converted"))
+  (s/config! convert-button :enabled? true))
 
 (defmulti build-menuitems-for (fn [x] x))
 (defmethod build-menuitems-for :file [_]
   [(s/menu-item :text "Add files"
-                :listen [:action (fn [_] 
-                                   (chooser/choose-file :type :open
-                                                        :multi? true
-                                                        :filters [["Images" ["png" "jpg" "jpeg"]]]
-                                                        :selection-mode :files-only
-                                                        :dir (get @prefs/prefs :dir)))])])
+                :listen [:action (fn [e] 
+                                   (choose-file :type :open
+                                                :multi? true
+                                                :filters [["Images" ["png" "jpg" "jpeg"]]]
+                                                :selection-mode :files-only
+                                                :dir (get @prefs/prefs :dir)
+                                                :success-fn files-selected))])])
 (defmethod build-menuitems-for :edit [_]
   [(s/menu-item :text "Preferences"
                 :listen [:action (fn [_] (prefs/make-prefs-dialog))])])
@@ -29,7 +42,9 @@
 (defn run []
   (s/native!)
   (def f (s/frame :title "Dandy" :menubar (build-menubar) :on-close :exit))
-  (def layout (s/grid-panel :rows 2 :columns 1))
+  (def layout (s/border-panel :border 5
+                              :hgap 5
+                              :vgap 5))
   (def buttons
     (let [group (s/button-group)]
       (s/grid-panel :rows 2 :columns 2
@@ -37,7 +52,8 @@
                             (s/radio :id :top-right :text "Top right" :group group)
                             (s/radio :id :bottom-left :text "Bottom left" :group group)
                             (s/radio :id :bottom-right :text "Bottom right" :group group)])))
-  (s/add! layout buttons)
-  (s/add! layout (s/button :text "Convert files" :enabled? false))
+  (s/add! layout [buttons :center])
+  (s/add! layout [convert-button :east])
+  (s/add! layout [status-text :south])
   (s/config! f :content layout)
   (-> f s/pack! s/show!))
--- src/dandy/prefs.clj
+++ src/dandy/prefs.clj
@@ -1,16 +1,15 @@
 (ns dandy.prefs
   (require [seesaw.core :as s]
-           [seesaw.pref :as prefs])
-  (use [seesaw.chooser :only (choose-file)]))
+           [seesaw.pref :as prefs]
+           [seesaw.forms])
+  (use [seesaw.chooser :only (choose-file)]
+       [seesaw.mig :only (mig-panel)]))
 
 (def prefs (prefs/preference-atom "prefs" {}))
-;; (println @prefs)
-;; (reset! prefs {:hello "world"})
-;; (println @prefs)
 
 (def directory-text-field
   (s/text :text (get @prefs :dir)
-          :columns 30
+          :columns 20
           :enabled? false))
 
 (defn dir-chosen [fc file]
@@ -18,33 +17,44 @@
     (reset! prefs (conj @prefs { :dir path }))
     (s/text! directory-text-field path)))
 
+(def directory-button
+  (s/button :text "Choose"
+            :listen [:action (fn [e]
+                               (choose-file :type :open
+                                            :selection-mode :dirs-only
+                                            :dir (get @prefs :dir)
+                                            :success-fn dir-chosen))]))
+
+(def layers-panel (s/horizontal-panel :border 5))
+
+(defn layer-chooser []
+  (choose-file :type :open :selection-mode :files-only))
+
+(def add-layer-pane
+  (mig-panel :items [[(s/text :text "Name" :columns 10) ""]
+                     [(s/button :text "Choose layer" :listen [:action (fn [e] (layer-chooser))]) ""]
+                     [(s/button :text "Add") ""]]))
+
 (defn make-layers-tab []
-  (s/border-panel))
+  (s/vertical-panel :items ["foo" "bar" "baz" "qux" "quuz"]))
+  ;; (seesaw.forms/forms-panel :items ["Login" (s/text) (seesaw.forms/next-line)
+  ;;                                   "Password" (seesaw.forms/span (s/text) 3)]))
+  ;; (mig-panel :items [[layers-panel "growy, wrap"]
+  ;;                    [add-layer-pane ""]]))
 
 (defn make-default-directory-tab []
-  (s/border-panel :border 5
-                  :hgap 5
-                  :vgap 5
-                  :center directory-text-field
-                  :east (s/button :text "Choose"
-                                  :listen [:action (fn [e]
-                                                     (choose-file :type :open
-                                                                  :selection-mode :dirs-only
-                                                                  :dir (get @prefs :dir)
-                                                                  :success-fn dir-chosen))])))
+  (mig-panel :items [[directory-text-field ""]
+                     [directory-button ""]]))
 
 (defn make-preference-tabs []
-  (s/tabbed-panel
-   :tabs [{ :title "Default directory" :content (make-default-directory-tab) }
-          { :title "Layers" :content (make-layers-tab) }]))
+  (mig-panel :items [[(s/tabbed-panel
+                       :tabs [{ :title "Default directory" :content (make-default-directory-tab) }
+                              { :title "Layers" :content (make-layers-tab) }])
+                      "grow, shrink"]]))
 
 (defn make-prefs-dialog []
   (let [frame (s/frame :title "Preferences"
                        :on-close :dispose
-                       :resizable? false)
-        layout (s/border-panel :border 5
-                               :hgap 5
-                               :vgap 5
-                               :center (make-preference-tabs))]
-    (s/config! frame :content layout)
+                       :resizable? false)]
+    (s/config! frame :content (make-preference-tabs))
     (-> frame s/pack! s/show!)))
