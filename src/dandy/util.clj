e71c98edf5f79b38f801e7902c683a457f357c50
Karl Sutt <karl@sutt.ee>
2013-10-02 23:23:49 +0100

    Refactor

--- src/dandy/util.clj
+++ src/dandy/util.clj
@@ -3,13 +3,14 @@
   (require [clojure.string :as str])
   (import javax.imageio.ImageIO))
 
+(def image-exts [".png" ".jpg" ".jpeg"])
+
+(defn ends-with-ext? [path ext]
+  (.endsWith (str/lower-case path) ext))
+
 (defn sanitise-path [path]
   (when
-      (some
-       (fn [ext]
-         (.endsWith (str/lower-case path) ext))
-       [".png" ".jpg" ".jpeg"])))
+      (some true? (map (ends-with-ext? path) image-exts)) path))
 
 (defn path->image [path]
   (-> path sanitise-path io/as-file ImageIO/read))
-
