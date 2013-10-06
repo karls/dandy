46a5621f612c7bba0ec05283d786dbf1c0405784
Karl Sutt <karl@sutt.ee>
2013-10-06 14:44:49 +0100

    Fix sanitisation

--- src/dandy/util.clj
+++ src/dandy/util.clj
@@ -3,14 +3,14 @@
   (require [clojure.string :as str])
   (import javax.imageio.ImageIO))
 
-(def image-exts [".png" ".jpg" ".jpeg"])
+(def image-exts (seq (ImageIO/getReaderFormatNames)))
 
 (defn ends-with-ext? [path ext]
   (.endsWith (str/lower-case path) ext))
 
 (defn sanitise-path [path]
   (when
-      (some true? (map (ends-with-ext? path) image-exts)) path))
+      (some true? (map #(ends-with-ext? path %1) image-exts)) path))
 
 (defn path->image [path]
   (-> path sanitise-path io/as-file ImageIO/read))
