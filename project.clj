e08feb17cc67e11f1d16c40b6b15d6515a2df98b
Karl Sutt <karl@sutt.ee>
2016-08-09 15:00:30 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/karls/dandy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [seesaw "1.4.3"]
-                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [seesaw "1.4.5"]
+                 [org.clojure/core.async "0.2.385"]
                  [com.github.insubstantial/substance "7.1"]]
   :profiles {:dev {:dependencies [[speclj "2.7.5"]]}}
   :plugins [[speclj "2.7.5"]]
